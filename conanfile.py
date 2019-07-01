d5ee6dab7e9b2f46ea600d025ba5a5cf1d3ea664
Jean Wicht <jean.wicht@pix4d.com>
2019-07-01 13:07:04 +0200

    Add revision number

--- conanfile.py
+++ conanfile.py
@@ -3,7 +3,9 @@ from conans import ConanFile, tools, CMake
 
 class PyBind11Conan(ConanFile):
     name = "pybind11"
-    version = "2.2.4"
+    upstream_version = "2.2.4"
+    revision = "1"
+    version = "{}-{}".format(upstream_version, revision)
     settings = "os", "compiler", "arch", "build_type"
     description = "Seamless operability between C++11 and Python"
     homepage = "https://github.com/pybind/pybind11"
@@ -12,17 +14,17 @@ class PyBind11Conan(ConanFile):
     no_copy_sources = True
 
     def source(self):
-        tools.get("%s/archive/v%s.tar.gz" % (self.homepage, self.version))
+        tools.get("%s/archive/v%s.tar.gz" % (self.homepage, self.upstream_version))
 
     def build(self):
         cmake = CMake(self)
         cmake.definitions["PYBIND11_TEST"] = False
-        cmake.configure(source_folder="pybind11-%s" % self.version)
+        cmake.configure(source_folder="pybind11-%s" % self.upstream_version)
         cmake.build()
         cmake.install()
 
     def package(self):
         self.copy("*LICENSE", keep_path=False)
-  
+
     def package_id(self):
         self.info.header_only()
