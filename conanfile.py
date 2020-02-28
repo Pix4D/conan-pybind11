30b0b3304c51c946181c3e5c3cb3e29006997eac
gs-pix4d <48286613+gs-pix4d@users.noreply.github.com>
2020-02-28 10:07:13 +0100

    Use Python 3.6 to build test package

--- conanfile.py
+++ conanfile.py
@@ -4,7 +4,7 @@ from conans import ConanFile, tools, CMake
 class PyBind11Conan(ConanFile):
     name = "pybind11"
     upstream_version = "2.4.3"
-    revision = "0"
+    revision = "1"
     version = "{}-{}".format(upstream_version, revision)
     settings = "os", "compiler", "arch", "build_type"
     description = "Seamless operability between C++11 and Python"
