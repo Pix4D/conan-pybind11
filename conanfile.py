f3719fba93c92c5cc97ffb187d7430e572d2dbe7
gs-pix4d <48286613+gs-pix4d@users.noreply.github.com>
2020-08-26 08:30:44 +0200

    Upgrade to version 2.5.0

--- conanfile.py
+++ conanfile.py
@@ -3,7 +3,7 @@ from conans import ConanFile, tools, CMake
 
 class PyBind11Conan(ConanFile):
     name = "pybind11"
-    upstream_version = "2.4.3"
+    upstream_version = "2.5.0"
     revision = "0"
     version = "{}-{}".format(upstream_version, revision)
     settings = "os", "compiler", "arch", "build_type"
